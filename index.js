97c6a4c0eb6a778e45ae111fc71f2c3e333180d4
Chris Manson <chris@manson.ie>
2019-12-05 13:49:09 +0000

    fixing default look of template

--- index.js
+++ index.js
@@ -1,5 +1,13 @@
 'use strict';
 
 module.exports = {
-  name: require('./package').name
+  name: require('./package').name,
+
+  contentFor(type, config) {
+    if (type === 'head') {
+      if(!config['field-guide'] || !config['field-guide'].logo) {
+        return '<link href="https://fonts.googleapis.com/css?family=Fredericka+the+Great&display=swap" rel="stylesheet">';
+      }
+    }
+  }
 };
